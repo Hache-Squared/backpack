6aefaf016c2370971a8a291be70cbdca43bda286
Angel Gabriel <aghh.oficial@gmail.com>
2024-06-30 02:17:15 -0600

    we have added icons in the app

--- src/explore/screens/BackpackScreen.tsx
+++ src/explore/screens/BackpackScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const BackpackScreen = () => {
   return (
     <View>
       <Text>Vista de mochila seleccionada</Text>
+      <Icon name="rocket-outline" size={30} color="#900" />
     </View>
   )
 }
