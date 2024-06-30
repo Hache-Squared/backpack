c4751fc6fe8911a4e91fa9e84ae653709e4399ff
Angel Gabriel <aghh.oficial@gmail.com>
2024-06-30 02:53:00 -0600

    have installed react-redux and reduxjs/toolkit

--- src/routes/TabNavigator.tsx
+++ src/routes/TabNavigator.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StackExplore } from './StackExplore';
 import { StackProfile } from './StackProfile';
 import { Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,6 +10,7 @@ export const TabNavigator = () => {
   return (
     <Tab.Navigator
         screenOptions={{
+            headerTitleAlign: 'center',
             headerStyle: {
                 elevation: 0,
                 borderColor: 'transparent',
@@ -23,8 +25,8 @@ export const TabNavigator = () => {
             }
         }}
     >
-      <Tab.Screen name="Explore" options={{ title: 'Explorar', tabBarIcon: ({ color }) => (<Text style={{color}}>icono</Text>) }} component={StackExplore} />
-      <Tab.Screen name="Profile" options={{ title: 'Perfil',   tabBarIcon: ({ color }) => (<Text style={{color}}>icono</Text>) }} component={StackProfile} />
+      <Tab.Screen name="Explore" options={{ title: 'Explorar', tabBarIcon: ({ color }) => ( <Icon name="school-outline" size={30} color={color} />) }} component={StackExplore} />
+      <Tab.Screen name="Profile" options={{ title: 'Perfil',   tabBarIcon: ({ color }) => ( <Icon name="happy-outline" size={30} color={color} /> ) }} component={StackProfile} />
     </Tab.Navigator>
   );
 }
