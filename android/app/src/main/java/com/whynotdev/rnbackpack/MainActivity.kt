a4bd2e80f2c7b1e481656f43ec0fe261babfdc76
Angel Gabriel <aghh.oficial@gmail.com>
2024-07-01 02:02:34 -0600

    we have rename the package in android

--- android/app/src/main/java/com/whynotdev/rnbackpack/MainActivity.kt
+++ android/app/src/main/java/com/whynotdev/rnbackpack/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.backpack
+package com.whynotdev.rnbackpack
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity
--- android/app/src/main/java/com/whynotdev/rnbackpack/MainApplication.kt
+++ android/app/src/main/java/com/whynotdev/rnbackpack/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.backpack
+package com.whynotdev.rnbackpack
 
 import android.app.Application
 import com.facebook.react.PackageList
